16430881af9e8ada1456483052389f9e10516495
benjaminhuang <benhua476@gmail.com>
2025-03-08 16:12:17 -0800

    refseq files made and done

--- refseq_id_extract.py
+++ refseq_id_extract.py
@@ -4,3 +4,9 @@ import pandas as pd
 df = pd.read_csv('idmapping_2025_03_08.tsv', sep='\t')
 data = df.values  # Convert to a NumPy array
 print(data)
+
+refseq_id_file = "refseq_ids.txt"
+file = open(refseq_id_file, 'w')
+for i,j in data:
+    file.write(str(j) + "\n")
+
